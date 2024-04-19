082c09b9e557fdecacf14e726dcee9702b9d79cc
Kokonod Ray <kokonodray2001@gmail.com>
2024-04-19 16:09:57 +0530

    commit 2 - using RequestParams for using parameters to get data

--- restApiStarter/src/main/java/com/thoughtworks/restapistarter/ui/controller/UserController.java
+++ restApiStarter/src/main/java/com/thoughtworks/restapistarter/ui/controller/UserController.java
@@ -3,13 +3,13 @@ package com.thoughtworks.restapistarter.ui.controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("users") // http://localhost:8080/users
+@RequestMapping("users") // http://localhost:8081/users
 public class UserController { // controller for user only
 
-    @GetMapping(path="/{userId}")
-    public String getUser(@PathVariable String userId)
+    @GetMapping
+    public String getUser(@RequestParam(value = "page") int page, @RequestParam(value = "limit") int limit)
     {
-        return "get user was called with userId: " + userId;
+        return "get user was called page " + page + " and limit " + limit;
     }
 
     @PostMapping
