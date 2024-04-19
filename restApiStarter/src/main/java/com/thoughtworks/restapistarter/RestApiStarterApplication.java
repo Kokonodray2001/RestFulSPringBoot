864e8f126e7598797d613d600b36e1f620b50e52
Kokonod Ray <kokonodray2001@gmail.com>
2024-04-19 11:44:48 +0530

    rest api starter

---
+++ restApiStarter/src/main/java/com/thoughtworks/restapistarter/RestApiStarterApplication.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.restapistarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiStarterApplication.class, args);
+		//System.out.println("Hello, World!");
+	}
+
+}
---
+++ restApiStarter/src/test/java/com/thoughtworks/restapistarter/RestApiStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.restapistarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
